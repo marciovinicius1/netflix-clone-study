a03bef96b26320f8ac2cb0e7a96728a43e8d2e4a
Márcio Vinícius de Jesus Santos <marcioviniciusdev@gmail.com>
2022-06-10 19:09:25 -0300

    account settings update

--- components/Header.tsx
+++ components/Header.tsx
@@ -46,14 +46,13 @@ function Header() {
         <SearchIcon className="hidden h-6 w-6 sm:inline" />
         <p className="hidden lg:inline">Kids</p>
         <BellIcon className="h-6 w-6" />
-        {/* <Link href="/account "> */}
-        <img
-          onClick={logout}
-          src="https://rb.gy/g1pwyx"
-          alt=""
-          className="cursor-pointer rounded"
-        />
-        {/* </Link> */}
+        <Link href="/account ">
+          <img
+            src="https://rb.gy/g1pwyx"
+            alt="user logo"
+            className="cursor-pointer rounded"
+          />
+        </Link>
       </div>
     </header>
   );
